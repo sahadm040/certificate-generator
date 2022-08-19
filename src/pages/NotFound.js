1f48127ad3d2f11aeb584d749dfdb18e7e219841
Rashid <rashidrashimr002@gmail.com>
2022-08-19 15:17:04 +0530

    line changed

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,19 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import img from  '../images/IMAGE.jpeg';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import img from "../images/IMAGE.jpeg";
 
 function NotFound() {
   return (
-    <div className='Not-Found' style={{height:"700px"}}>
-      <h1 style={{display:"flex",alignItems:'center',justifyContent:'center'}}>Sorry</h1>
-      <h2 style={{display:"flex",alignItems:'center',justifyContent:'center'}}>That page is cannot found</h2>
-      <div style={{display:"flex",alignItems:'center',justifyContent:'center'}} >
-      <img  src={img} alt="" />
+    <div className="Not-Found" style={{ height: "700px" }}>
+      <h1
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Sorry
+      </h1>
+      <h2
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        That page is cannot found
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img src={img} alt="" />
       </div>
-     
-      <Link style={{display:"flex",alignItems:'center',justifyContent:'center'}} to="/home"> Back to home page......</Link> 
-      </div>
-  )
+
+      <Link
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        to="/home"
+      >
+        {" "}
+        Back to home page......
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
